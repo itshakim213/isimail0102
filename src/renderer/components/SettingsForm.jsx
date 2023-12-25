d6004346eeba519031a13dafa7efc8f8f350b57e
samia abbadja <samiaabbadja@gmail.com>
2023-12-25 09:07:50 +0100

    settings added

--- src/renderer/components/SettingsForm.jsx
+++ src/renderer/components/SettingsForm.jsx
@@ -4,10 +4,10 @@ function SettingsForm() {
   return (
     <form className="settings-form">
       <div className="setting-box">
-        <label className="option">Theme</label>
+        <label className="option">Théme</label>
         <div className="values">
           <input type="radio" id="default" name="theme" />
-          <label for="default">Par defaut</label>
+          <label for="default">Par défaut</label>
         </div>
         <div className="values">
           <input type="radio" id="light" name="theme" />
@@ -20,7 +20,7 @@ function SettingsForm() {
       </div>
 
       <div className="setting-box">
-        <label className="option">Action pour les notification</label>
+        <label className="option">Action pour les notifications </label>
         <div className="values">
           <input type="radio" id="archiver" name="notAction" />
           <label for="archiver">Archiver</label>
@@ -34,10 +34,11 @@ function SettingsForm() {
       <div className="setting-box">Gestion des notification</div>
 
       <div className="setting-box">
-        <label className="option">Densite de la list des conversation</label>
+        <label className="option">Densité de la liste des conversations </label>
+
         <div className="values">
           <input type="radio" id="parDef" name="densite" />
-          <label for="parDef">Par defaut</label>
+          <label for="parDef">Par défaut</label>
         </div>
         <div className="values">
           <input type="radio" id="normal" name="densite" />
@@ -50,44 +51,35 @@ function SettingsForm() {
       </div>
 
       <div className="setting-box">
-        <label className="option">Type de reponse au mails/chats</label>
+        <label className="option">Type de réponse au mails/chats</label>
         <div className="values">
           <input type="radio" id="rep" name="reponse" />
-          <label for="rep">Repondre</label>
+          <label for="rep">Répondre</label>
         </div>
         <div className="values">
           <input type="radio" id="repTous" name="reponse" />
-          <label for="repTous">Repondre a tous</label>
+          <label for="repTous">Répondre à tous</label>
         </div>
       </div>
+
       <div className="setting-checkbox">
+
         <label className="option">Confirmer les actions</label>
         <div className="values">
-          <input type="radio" id="suppression" name="suppression" value="suppression" />
-          <label htmlFor="suppression">Confirmer avant suppression</label>
+          <input type="checkbox" id="suppression" name="suppression" />
+          <label for="suppression">Confirmer avant suppression</label>
         </div>
         <div className="values">
-          <input type="radio" id="archivage" name="archivage" value ="archivage" />
-          <label htmlFor="archivage">Confirmer avant archivage</label>
+          <input type="checkbox" id="archivage" name="archivage" />
+          <label for="archivage">Confirmer avant archivage</label>
         </div>
         <div className="values">
-          <input type="radio" id="envoi" name="envoi" value = "envoi"/>
-          <label htmlFor="envoi">Confirmer avant l'envoie</label>
+          <input type="checkbox" id="envoi" name="envoi" />
+          <label for="envoi">Confirmer avant l'envoie</label>
         </div>
       </div>
-      <script>
-  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-  checkboxes.forEach(checkbox => {
-    checkbox.addEventListener('change', function() {
-      checkboxes.forEach(cb => {
-        if (cb !== this) {
-          cb.checked = false;
-        }
-      });
-    });
-  });
-</script>
     </form>
+
   );
 }
 export default SettingsForm;
