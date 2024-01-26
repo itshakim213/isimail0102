a0137f00ea85e6bc2393148edb3db4e4e6dcdd37
liliiiiiii2 <lilitil123@gmail.com>
2024-01-27 00:21:38 +0100

    chat added

--- src/renderer/components/PageSelectCont.jsx
+++ src/renderer/components/PageSelectCont.jsx
@@ -1,51 +1,92 @@
-import React from 'react';
 import Empty from './Empty';
 import noSelect from '../assets/noSelected.png';
 import Newmessage from './Newmessage';
-import EmailDetailsModal from '../components/EmailDetailsModal';
 import '../styles/PageSelectCont.css';
+import { useContext } from 'react';
+import { ChatState } from '../context/ChatContext';
+import SingleChat from './SingleChat';
+import { useLocation } from 'react-router-dom';
+import ProfileModal from './ProfileModal';
+import { getSenderFull } from '../context/ChatLogics';
+import UpdateGroupChatModal from './UpdateGroupChatModal';
+import EmailDetailsModal from './EmailDetailsModal';
 
-function PageSelectCont({ message, showNewMessage, showNewMessageForm, isEmailModalOpen,
+function PageSelectCont({
+  fetchAgain,
+  setFetchAgain,
+  message,
+  showNewMessage,
+  showNewMessageForm,
+  isEmailModalOpen,
   closeEmailModal,
   setShowNewMessage,
   setReply,
   reply,
   fwd,
-  setFwd, }) {
-  return (
-    <div
-      className="page-content"
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        padding: '34px 65px',
-      }}
-    >
-      {showNewMessage ? (
-        <Newmessage reply={reply} fwd={fwd} />
-      ) : (
-        <>
-          {selectedEmail ? (
-            <EmailDetailsModal
-              emailInfo={selectedEmail}
-              isModalOpen={isEmailModalOpen}
-              handleClose={closeEmailModal}
-              setReply={setReply}
-              setShowNewMessage={setShowNewMessage}
-              setFwd={setFwd}
-            />
+  setFwd,
+  selectedEmail,
+}) {
+  const { selectedChat, user } = useContext(ChatState);
+  let locat = useLocation().pathname;
+  let part = locat.split('/');
+  if (part[1] === 'chats') {
+    return (
+      <div
+        className="page-list"
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          paddingTop: 0,
+        }}
+      >
+        {selectedChat ? (
+          !selectedChat.isGroupChat ? (
+            <ProfileModal user={getSenderFull(user, selectedChat.users)} />
           ) : (
-            <Empty
-              image={noSelect}
-              message={message}
-              width={180}
-              height={180}
+            <UpdateGroupChatModal
+              fetchAgain={fetchAgain}
+              setFetchAgain={setFetchAgain}
             />
-          )}
-        </>
-      )}
-    </div>
-  );
+          )
+        ) : (
+          <Empty image={noSelect} message={message} width={180} height={180} />
+        )}
+      </div>
+    );
+  } else
+    return (
+      <div
+        className="page-content"
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          padding: '34px 65px',
+        }}
+      >
+        {showNewMessage ? (
+          <Newmessage reply={reply} fwd={fwd} />
+        ) : (
+          <>
+            {selectedEmail ? (
+              <EmailDetailsModal
+                emailInfo={selectedEmail}
+                isModalOpen={isEmailModalOpen}
+                handleClose={closeEmailModal}
+                setReply={setReply}
+                setShowNewMessage={setShowNewMessage}
+                setFwd={setFwd}
+              />
+            ) : (
+              <Empty
+                image={noSelect}
+                message={message}
+                width={180}
+                height={180}
+              />
+            )}
+          </>
+        )}
+      </div>
+    );
 }
-
 export default PageSelectCont;
